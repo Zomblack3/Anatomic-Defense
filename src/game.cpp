872fe5cb2e1dc1822cc1925eb9ef5206a5d0cc6c
Zomblack3 <santi260406@gmail.com>
2025-10-09 22:30:51 -0300

    game loop setted
    The game loop structure was added to game function
    The window size was setted
    The game's screens were created

--- src/game.cpp
+++ src/game.cpp
@@ -1,11 +1,52 @@
 #include "game.h"
 
-#include "raylib.h"
+using namespace settings;
 
 namespace run
 {
 	void game()
 	{
-		
+		Screen currentScreen = Screen::MAIN_MENU;
+
+		InitWindow(screenWidth, screenHeight, "Anatomic Defense");
+
+		while (!WindowShouldClose())
+		{
+			switch (currentScreen)
+			{
+			case Screen::MAIN_MENU:
+
+				
+
+				break;
+			case Screen::GAMEPLAY:
+
+
+
+				break;
+			case Screen::WIN_SCREEN:
+
+
+
+				break;
+			case Screen::LOSE_SCREEN:
+
+
+
+				break;
+			case Screen::OPTIONS:
+
+
+
+				break;
+			case Screen::EXIT:
+
+
+
+				break;
+			default:
+				break;
+			}
+		}
 	}
 }
--- src/game.h
+++ src/game.h
@@ -1,6 +1,24 @@
 #pragma once
 
+#include <raylib.h>
+
 namespace run
 {
 	void game();
+}
+
+namespace settings
+{
+	constexpr int screenWidth = 1250;
+	constexpr int screenHeight = 750;
+
+	enum class Screen
+	{
+		MAIN_MENU,
+		GAMEPLAY,
+		WIN_SCREEN,
+		LOSE_SCREEN,
+		OPTIONS,
+		EXIT
+	};
 }
